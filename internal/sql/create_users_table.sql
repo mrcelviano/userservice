2352dd6bd7fcdcf0d9799c61da1e3963c8250ed1
mr_celviano <nikita.sergienko@enkod.io>
2022-02-13 16:57:17 +0300

    add_sql

--- internal/sql/create_users_table.sql
+++ internal/sql/create_users_table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
     id serial4 NOT NULL,
-    email text NOT NULL DEFAULT ''::text unique,
-    "name" text NOT NULL DEFAULT ''::text unique,
     is_deleted bool NOT NULL DEFAULT false,
+    email text NOT NULL DEFAULT ''::text,
     is_registered bool NOT NULL DEFAULT false,
+    "name" text NOT NULL DEFAULT ''::text,
+    CONSTRAINT users_email_key UNIQUE (email),
     CONSTRAINT users_pkey PRIMARY KEY (id)
 );
