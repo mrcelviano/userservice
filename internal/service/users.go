063c3cae2a4d83f2d182b593d77e2dbdf54d9514
mr_celviano <nikita.sergienko@enkod.io>
2022-02-13 14:57:42 +0300

    add_sql

--- internal/service/users.go
+++ internal/service/users.go
@@ -33,7 +33,7 @@ func (u *userService) Create(ctx context.Context, user domain.User) (domain.User
 
 	_, err = u.notification.SendNotification(ctx, newUser.ID, newUser.Email, newUser.Name)
 	if err != nil {
-		logger.Info("can`t send notification. Error: ", err.Error())
+		logger.Infof("can`t send notification: %s\n", err.Error())
 	}
 	return newUser, nil
 }
@@ -41,7 +41,7 @@ func (u *userService) Create(ctx context.Context, user domain.User) (domain.User
 func (u *userService) Update(ctx context.Context, user domain.User) (domain.User, error) {
 	resp, err := u.userPG.Update(ctx, user)
 	if err != nil {
-		logger.Debug(err.Error())
+		logger.Errorf("can`t update user: %s\n", err.Error())
 		return resp, domain.ErrInternalServerError
 	}
 	return resp, nil
@@ -50,7 +50,7 @@ func (u *userService) Update(ctx context.Context, user domain.User) (domain.User
 func (u *userService) GetByID(ctx context.Context, id int64) (domain.User, error) {
 	resp, err := u.userPG.GetByID(ctx, id)
 	if err != nil {
-		logger.Debug(err.Error())
+		logger.Errorf("can`t get user by id: %s\n", err.Error())
 		return resp, domain.ErrInternalServerError
 	}
 	return resp, nil
@@ -60,14 +60,14 @@ func (u *userService) GetList(ctx context.Context, p domain.GetUserListRequest) 
 	logger.Info("getting user list")
 	userList, err := u.userPG.GetList(ctx, p)
 	if err != nil {
-		logger.Debug(err.Error())
+		logger.Errorf("can`t get user list: %s\n", err.Error())
 		return domain.GetUserListResponse{}, domain.ErrInternalServerError
 	}
 
 	logger.Info("get total ef")
 	userTotal, err := u.userPG.GetTotal(ctx)
 	if err != nil {
-		logger.Debug(err.Error())
+		logger.Errorf("can`t get total user: %s\n", err.Error())
 		return domain.GetUserListResponse{}, domain.ErrInternalServerError
 	}
 	return domain.GetUserListResponse{
@@ -79,7 +79,7 @@ func (u *userService) GetList(ctx context.Context, p domain.GetUserListRequest) 
 func (u *userService) Delete(ctx context.Context, id int64) error {
 	err := u.userPG.Delete(ctx, id)
 	if err != nil {
-		logger.Debug(err.Error())
+		logger.Errorf("can`t delete user: %s\n", err.Error())
 		return domain.ErrInternalServerError
 	}
 	return nil
